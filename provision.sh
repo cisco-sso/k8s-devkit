a0d126cdbaf71eb26f4ccc3c66bb883548297f1d
David C Wang <dcwangmit01@gmail.com>
2018-05-14 17:12:18 +0000

    Reworked dotfiles to use yadm
    * Deployed dotfiles via yadm
    * Removed all old dotfile symlinking logic
    * Removed the chown of the vagrant home dir.  Fixed on the
      other side in ansible-role-k8s-devkit.

--- provision.sh
+++ provision.sh
@@ -34,68 +34,6 @@ sudo pip3.6 install -r requirements/pip3.6.txt
 echo "## Clean up yum metadata which may become stale during Vagrant box distribution."
 sudo yum clean all --quiet
 
-echo "## Persist the configuration directories for several tools."
-#  Do not include:
-#     ["/vagrant/dotfiles/dot.helm"]="/home/vagrant/.helm" \
-#     ["/vagrant/dotfiles/dot.minikube"]="/home/vagrant/.minikube" \
-declare -A from_to_dirs
-from_to_dirs=( \
-    ["/vagrant/dotfiles/dot.ssh"]="/home/vagrant/.ssh" \
-    ["/vagrant/dotfiles/dot.aws"]="/home/vagrant/.aws" \
-    ["/vagrant/dotfiles/dot.docker"]="/home/vagrant/.docker" \
-    ["/vagrant/dotfiles/dot.emacs.d"]="/home/vagrant/.emacs.d" \
-    ["/vagrant/dotfiles/dot.gnupg"]="/home/vagrant/.gnupg" \
-    ["/vagrant/dotfiles/dot.gcloud"]="/home/vagrant/.config/gcloud" \
-    ["/vagrant/dotfiles/dot.govc"]="/home/vagrant/.govc" \
-    ["/vagrant/dotfiles/dot.mc"]="/home/vagrant/.mc" \
-    ["/vagrant/dotfiles/dot.openstack"]="/home/vagrant/.config/openstack" \
-    ["/vagrant/dotfiles/dot.kube"]="/home/vagrant/.kube" )
-for from_dir in "${!from_to_dirs[@]}"; do
-    to_dir=${from_to_dirs[$from_dir]}
-    ### Ensure dotfiles config directory exists.
-    if [ ! -d "${from_dir}" ]; then
-        mkdir -p ${from_dir}
-    fi
-    ### Set link to the dotfiles config directory.
-    if [ ! -e $to_dir ]; then
-        mkdir -p `dirname $to_dir`
-        ln -s $from_dir $to_dir
-    fi
-done
-
-echo "## Persist the configuration files for several tools."
-declare -A from_to_files
-#  Fails on Windows10
-#  ["/vagrant/dotfiles/dot.ssh/config"]="/home/vagrant/.ssh/config" \
-from_to_files=( \
-  ["/vagrant/dotfiles/dot.gitconfig"]="/home/vagrant/.gitconfig" \
-  ["/vagrant/dotfiles/dot.hub"]="/home/vagrant/.config/hub" \
-  ["/vagrant/dotfiles/dot.emacs"]="/home/vagrant/.emacs" \
-  ["/vagrant/dotfiles/dot.gitignore"]="/home/vagrant/.gitignore" \
-  ["/vagrant/dotfiles/dot.screenrc"]="/home/vagrant/.screenrc" \
-  ["/vagrant/dotfiles/dot.nova"]="/home/vagrant/.nova" \
-  ["/vagrant/dotfiles/dot.openstack"]="/home/vagrant/.config/openstack" \
-  ["/vagrant/dotfiles/dot.supernova"]="/home/vagrant/.supernova" \
-  ["/vagrant/dotfiles/dot.superglance"]="/home/vagrant/.superglance" \
-  ["/vagrant/dotfiles/dot.vimrc"]="/home/vagrant/.vimrc" \
-  ["/vagrant/dotfiles/dot.tmux.conf"]="/home/vagrant/.tmux.conf" \
-  ["/vagrant/dotfiles/dot.tmux.conf.local"]="/home/vagrant/.tmux.conf.local" )
-
-for from_file in "${!from_to_files[@]}"; do
-  to_file=${from_to_files[$from_file]}
-  ### Ensure dotfiles config file exists and is empty.
-  if [ ! -f ${from_file} ]; then
-    mkdir -p `dirname $from_file`
-    touch $from_file
-  fi
-  ### Set link to the dotfiles config file.
-  if [ ! -L $to_file ] || [ ! -e $to_file ]; then
-    rm -f $to_file
-    mkdir -p `dirname $to_file`
-    ln -s $from_file $to_file
-  fi
-done
-
 echo "## Symlink config.yaml to vagrant homedir."
 if [[ ! -L ~vagrant/config.yaml ]] ; then
   ln -s /vagrant/config.yaml ~vagrant/config.yaml
@@ -112,8 +50,15 @@ sudo \
   /vagrant/ansible/main.sh ${@}
 
 echo "  "  # Highlight Ansible results.
-echo "## Ensure vagrant owns everything in its home dir."
-sudo chown -R 1000:1000 /home/vagrant
+
+
+# echo "## Ensure vagrant owns everything in its home dir."
+# sudo chown -R 1000:1000 /home/vagrant
+
+echo "## Setup default dotfiles"
+if [[ ! -d /home/vagrant/.yadm ]]; then
+  sudo -i -u vagrant yadm clone https://github.com/dcwangmit01/yadm-dotfiles.git
+fi
 
 echo "## Clean up yum metadata which may become stale during Vagrant box distribution."
 sudo yum clean all --quiet
